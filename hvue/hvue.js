04f0590a58a85bff74d458c1a1c5bffdfb938755
Heyafeng <>
2021-01-15 19:18:21 +0800

    add

--- hvue/hvue.js
+++ hvue/hvue.js
@@ -1,27 +1,29 @@
+/* eslint-disable */
 class HVue {
   constructor(options) {
     // 保存选项
-    this.$options = options
-    this.$data = options.data
-
+    this.$options = options;
+    this.$data = options.data;
+    
     // 数据响应式处理
-    observe(this.$data)
+    observe(this.$data);
 
     // 代理data到hvue实例上
-    proxy(this)
+    proxy(this);
 
     // 编译
-    new Compile(options.el, this)
+    new Compile(options.el, this);
   }
 }
 
 class Compile {
   constructor(el, vm) {
-    this.$el = document.querySelector(el)
-    this.$vm = vm
+    this.$el = document.querySelector(el);
+    this.$vm = vm;
+    console.log(this.$vm, '#vmmm')
 
     // 根据dom渲染
-    this.compile(this.$el)
+    this.compile(this.$el);
   }
 
   /**
@@ -29,24 +31,24 @@ class Compile {
    * @param {Object} el el dom树
    */
   compile(el) {
-    el.childNodes.forEach(node => {
+    el.childNodes.forEach((node) => {
       if (this.isElement(node)) { // 如果是元素
         // console.log("编译元素", node.nodeName);
 
         // 处理属性和子节点
-        this.compileElement(node)
+        this.compileElement(node);
 
         // 递归子节点
         if (node.childNodes && node.childNodes.length) {
-          this.compile(node)
+          this.compile(node);
         }
       } else if (this.isInter(node)) { // 如果是文本
-        console.log("编译插值表达式", node.textContent);
+        console.log('编译插值表达式', node.textContent);
 
         // 获取表达式的值并赋值给node
-        this.compileText(node)
+        this.compileText(node);
       }
-    })
+    });
   }
 
   /**
@@ -54,7 +56,7 @@ class Compile {
    * @param {Object} node 节点
    */
   isElement(node) {
-    return node.nodeType === 1
+    return node.nodeType === 1;
   }
 
   /**
@@ -70,20 +72,26 @@ class Compile {
    * @param {String} attr 属性名称
    */
   isDir(attr) {
-    return attr.startsWith('h-')
+    return attr.startsWith('h-');
   }
 
+  /**
+   * 判断是否是@事件
+   */
+  isEvent(attr) {
+    return attr.startsWith('@')
+  }
 
   update(node, exp, dir) {
     // init
     // 每一个指令都有一个updater方法，比如h-text有textUpdater方法，该方法接收2个参数，node和值
-    const fn = this[dir + 'Updater']
-    fn && fn(node, this.$vm[exp])
+    const fn = this[`${dir}Updater`];
+    fn && fn(node, this.$vm[exp]);
 
     // update: 创建watcher
-    new Watcher(this.$vm, exp, function(val) {
-      fn && fn(node, val)
-    })
+    new Watcher(this.$vm, exp, ((val) => {
+      fn && fn(node, val);
+    }));
   }
 
   /**
@@ -92,48 +100,62 @@ class Compile {
    */
   compileElement(node) {
     // node.attributes是一个类数组对象 需先转换成数组
-    Array.from(node.attributes).forEach(attr => {
-      console.log(attr)
-      const attrName = attr.name
-      const exp = attr.value
+    Array.from(node.attributes).forEach((attr) => {
+      const attrName = attr.name;
+      const exp = attr.value;
 
       // 判断是否是一个指令
       if (this.isDir(attrName)) {
         // 执行指令处理函数
         // h-text 关心text
-        const dir = attrName.substring(2)
-        this[dir] && this[dir](node, exp)
+        const dir = attrName.substring(2);
+        this[dir] && this[dir](node, exp);
+        
+      } else if (this.isEvent(attrName)) { // 如果是以@开头的事件
+        // 拿到事件
+        const eventName = attrName.substring(1);
+        // 添加事件监听， 方法要注意this指向
+        const enentFn = this.$vm.$options.methods[exp]
+        node.addEventListener(eventName, enentFn.bind(this.$vm))
       }
-    })
+    });
   }
 
   // 编译文本
   compileText(node) {
-    this.update(node, RegExp.$1, 'text')
+    this.update(node, RegExp.$1, 'text');
   }
 
   // h-text处理函数
   text(node, exp) {
-    this.update(node, exp, 'text')
+    this.update(node, exp, 'text');
   }
 
   textUpdater(node, val) {
-    node.textContent = val
+    node.textContent = val;
   }
 
   // h-html处理函数
   html(node, exp) {
-    this.update(node, exp, 'html')
+    this.update(node, exp, 'html');
   }
 
   htmlUpdater(node, val) {
-    node.innerHTML = val
+    node.innerHTML = val;
+  }
+
+  model(node, exp) {
+    this.update(node, exp, 'model');
+  }
+
+  modelUpdater(node, val) {
+    node.innerText = val
   }
 }
 
 /**
  * 递归遍历obj， 动态拦截obj的所有的key
- * @param {Object} obj 
+ * @param {Object} obj
  */
 function observe(obj) {
   if (typeof obj !== 'object' || obj === null) {
@@ -141,7 +163,7 @@ function observe(obj) {
   }
 
   // 每出现一个对象 创建一个Observer实例
-  new Observer(obj)
+  new Observer(obj);
 }
 
 /**
@@ -149,16 +171,16 @@ function observe(obj) {
  * @param {Object} vm 上下文 this
  */
 function proxy(vm) {
-  Object.keys(vm.$data).forEach(key => {
+  Object.keys(vm.$data).forEach((key) => {
     Object.defineProperty(vm, key, {
       get() {
-        return vm.$data[key]
+        return vm.$data[key];
       },
       set(v) {
-        vm.$data[key] = v
-      }
-    })
-  })
+        vm.$data[key] = v;
+      },
+    });
+  });
 }
 
 /**
@@ -166,12 +188,12 @@ function proxy(vm) {
  */
 class Observer {
   constructor(obj) {
-    this.value = obj
+    this.value = obj;
 
     if (Array.isArray(obj)) {
       // todo
     } else {
-      this.walk(obj)
+      this.walk(obj);
     }
   }
 
@@ -180,48 +202,47 @@ class Observer {
    * @param {Object} obj 传入的对象
    */
   walk(obj) {
-    Object.keys(obj).forEach(k => {
-      defineReactive(obj, k, obj[k])
-    })
+    Object.keys(obj).forEach((k) => {
+      defineReactive(obj, k, obj[k]);
+    });
   }
 }
-
 
 class Watcher {
   constructor(vm, key, updateFn) {
     // hvue实例
-    this.vm = vm
+    this.vm = vm;
     // 依赖key
-    this.key = key
+    this.key = key;
     // 更新函数
-    this.updateFn = updateFn
+    this.updateFn = updateFn;
 
     // 读取key的值，触发get 收集依赖
-    Dep.target = this
-    this.vm[this.key]
-    Dep.target = null
+    Dep.target = this;
+    this.vm[this.key];
+    Dep.target = null;
   }
 
   // 更新
   update() {
     // 改变hvue依赖的key的指向
-    this.updateFn.call(this.vm, this.vm[this.key])
+    this.updateFn.call(this.vm, this.vm[this.key]);
   }
 }
 
 class Dep {
   constructor() {
-    this.deps = []
+    this.deps = [];
   }
 
   addDep(dep) {
-    this.deps.push(dep)
+    this.deps.push(dep);
   }
 
   notify() {
-    this.deps.forEach(dep => {
-      dep.update()
-    })
+    this.deps.forEach((dep) => {
+      dep.update();
+    });
   }
 }
 
@@ -235,29 +256,28 @@ function defineReactive(obj, key, val) {
   // * 如果原始对象的值是对象，那么还是需要递归遍历该值
   observe(val);
 
-  const dep = new Dep()
+  const dep = new Dep();
   // - 要拦截的对象obj
   // - 要设置的属性名称
   // - descriptor属性描述器，是一个对象，里面有set和get方法
   Object.defineProperty(obj, key, {
     get() {
       // 返回的是最新的值
-      console.log('get key', key); // sy-log
+      // console.log('get key', key); // sy-log
       // 依赖收集
-      Dep.target && dep.addDep(Dep.target)
+      Dep.target && dep.addDep(Dep.target);
       return val;
     },
     set(v) {
       // 如果老的值与新传进来的值不相等 设置为最新的值。这样get里面拿到的就是最新的值
       if (val !== v) {
-        console.log('set key', key); // sy-log
+        // console.log('set key', key); // sy-log
         // 因为传入的新值v可能还是一个对象， 所以需要遍历。
         observe(v);
         val = v;
 
-        dep.notify()
+        dep.notify();
       }
     },
   });
 }
-
